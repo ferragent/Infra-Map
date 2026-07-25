aee569930aa9629b85c5cf65d39d46d22a110feb
Flemin Paulson <56766497+ferragent@users.noreply.github.com>
2026-07-26 00:05:46 +0530

    Switch backend.tf to S3-native locking (use_lockfile) - no DynamoDB table needed

--- aws-432874556510/backend.tf
+++ aws-432874556510/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-fleminp"
-    key            = "global/terraform.tfstate"
-    region         = "ca-central-1"
-    dynamodb_table = "tf-lock-fleminp"
-    encrypt        = true
+    bucket       = "tf-state-fleminp"
+    key          = "global/terraform.tfstate"
+    region       = "ca-central-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
