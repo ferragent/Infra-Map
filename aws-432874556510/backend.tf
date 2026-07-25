63659e97612964c829a5475005b1e53ad00bddba
Flemin Paulson <56766497+ferragent@users.noreply.github.com>
2026-07-26 01:59:01 +0530

    Delete aws-432874556510 directory

--- aws-432874556510/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tf-state-fleminp"
-    key          = "global/terraform.tfstate"
-    region       = "ca-central-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
--- aws-432874556510/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "account_id" {
-  description = "The AWS account ID"
-  value       = data.aws_caller_identity.current.account_id
-}
-
-output "region" {
-  description = "The AWS region used by the provider"
-  value       = data.aws_region.current.name
-}
