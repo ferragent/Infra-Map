8ff945b2be8b8a1c205496abc5b60ec933763d34
Flemin Paulson <56766497+ferragent@users.noreply.github.com>
2026-07-27 17:46:52 +0530

    Ferragent AI scaffold: fleminp (#17)

--- fleminp/variables.tf
+++ fleminp/variables.tf
@@ -1,5 +1,11 @@
 variable "aws_region" {
   type        = string
-  description = "AWS region"
+  description = "The AWS region to use for the provider"
   default     = "ca-central-1"
+}
+
+variable "iam_username" {
+  type        = string
+  description = "The name of the IAM user to import"
+  default     = "ferragent-user"
 }
