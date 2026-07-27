5474ec0fdc8edad8a2b6690f604c28cb6a60a34e
Flemin Paulson <56766497+ferragent@users.noreply.github.com>
2026-07-27 17:37:56 +0530

    Ferragent AI scaffold: fleminp

--- fleminp/main.tf
+++ fleminp/main.tf
@@ -1,9 +1,3 @@
-# terraform import aws_iam_user.ferragent-user ferragent-user
-resource "aws_iam_user" "ferragent-user" {
+resource "aws_iam_user" "example" {
   name = "ferragent-user"
 }
-
-# terraform import aws_iam_group.example my-group
-# resource "aws_iam_group" "example" {
-#   name = "my-group"
-# }
