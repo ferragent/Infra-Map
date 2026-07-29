9b0066a9121d7e6f18d5057158446f3caad4e8f3
Flemin Paulson <56766497+ferragent@users.noreply.github.com>
2026-07-29 08:06:47 +0530

    Ferragent AI scaffold: fleminp (#22)

--- fleminp/main.tf
+++ fleminp/main.tf
@@ -1,7 +1,7 @@
-resource "aws_iam_user" "example" {
-  # will be populated automatically by the import
+resource "aws_iam_user" "ferragent-user" {
+  name = "ferragent-user"
 }
 
-resource "aws_s3_bucket" "example" {
-  # will be populated automatically by the import
+resource "aws_s3_bucket" "tf-state-fleminp" {
+  bucket = "tf-state-fleminp"
 }
