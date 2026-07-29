eff20d26b0c35ff0c9bd64a3749db64d240330b1
Flemin Paulson <56766497+ferragent@users.noreply.github.com>
2026-07-29 08:04:09 +0530

    Ferragent AI scaffold: fleminp (#21)

--- fleminp/backend.tf
+++ fleminp/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-fleminp"
-    key    = "fleminp/terraform.tfstate"
-    region = "ca-central-1"
-    use_lockfile = true
+    bucket         = "tf-state-fleminp"
+    key            = "fleminp/terraform.tfstate"
+    region         = "ca-central-1"
+    use_lockfile   = true
   }
 }
 
